be4957a94b687a4782f9b5ba90ee685edca628c7
Thanadon Inthasit <pleum-20013@hotmail.com>
2020-01-20 23:33:47 +0700

    fix color

--- admin/gamecontrol.js
+++ admin/gamecontrol.js
@@ -3,7 +3,7 @@ let text = "previous guess : "
 let previous = document.querySelector("#previous").textContent = text
 document.querySelector("button[name=refresh]").style.visibility="hidden"
 var numberOftimes = 0
-var timeToCountdown =5
+var timeToCountdown =60
 var seconds = timeToCountdown
 
 timer()
@@ -14,13 +14,16 @@ timer()
             numberOftimes++
             if(numberOftimes<10 && seconds !==0){
             if(gettext>number){
+                document.querySelector("#status").setAttribute("class","callout callout-warning")
                 document.querySelector("#game").textContent = "Go wrong"
                 document.querySelector("#status").textContent = "Too High"
-                document.querySelector("#game").setAttribute("class", "democlass");
+                // document.querySelector("#game").setAttribute("class", "democlass");
             }else if (gettext<number){
+                document.querySelector("#status").setAttribute("class","callout callout-info")
                 document.querySelector("#game").textContent = "Go wrong"
                 document.querySelector("#status").textContent = "Too Low"
             }else{
+                document.querySelector("#status").setAttribute("class","calloutcallout-success")
                 document.querySelector("#game").textContent = "Congratulation"
                 document.querySelector("#status").textContent = ""
             }
@@ -29,6 +32,7 @@ timer()
             document.querySelector("input[name=guess]").value = ""
             document.querySelector("button[name=refresh]").style.visibility="visible"
         }else{
+            document.querySelector("#status").setAttribute("class","callout callout-success")
             document.querySelector("#game").textContent = "Game Over"
             document.querySelector("button[name=confirm]").disabled = "true"
             document.querySelector("input[name=guess]").disabled = "true"
@@ -54,8 +58,8 @@ timer()
         document.querySelector("button[name=confirm]").disabled = false
         document.querySelector("input[name=guess]").disabled = false
         document.querySelector("button[name=start]").style.visibility = "visible"
-        document.querySelector("form[name=from]").style.visibility = "hidden"
-        document.querySelector("form[name=from]").style.visibility = "hidden"
+        // document.querySelector("form[name=from]").style.visibility = "hidden"
+        document.querySelector(".inputform").style.visibility = "hidden"
         document.querySelector("#previous").style.visibility = "hidden"
         document.querySelector("#timer").style.visibility = "hidden"
         document.querySelector("#game").style.visibility = "hidden"
@@ -65,8 +69,8 @@ function start(){
     seconds = timeToCountdown
     document.querySelector("#timer").style.visibility = "visible"
     document.querySelector("#game").style.visibility = "visible"
-    document.querySelector("button[name=start]").style.visibility = "hidden"
-    document.querySelector("form[name=from]").style.visibility = "visible"
+    // document.querySelector("button[name=start]").style.visibility = "hidden"
+    document.querySelector(".inputform").style.visibility = "visible"
     document.querySelector("#previous").style.visibility = "visible"
 }
 
